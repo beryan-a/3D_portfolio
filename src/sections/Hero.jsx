82cddef16a1dc1d0175471f2bd77531e4972e24e
Beryan Albeik <beryanalbeik@gmail.com>
2026-08-05 16:59:15 +0300

    fixes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 //import AnimatedCounter from "../components/AnimatedCounter";
-//import Button from "../components/Button";
+import Button from "../components/Button";
 import { words } from "../constants";
 //import HeroExperience from "../components/models/hero_models/HeroExperience";
 
