879979c460cb0307a94637d381a907b071309970
Beryan Albeik <beryanalbeik@gmail.com>
2026-08-06 13:54:04 +0300

    room lightning setting

--- src/components/models/hero_models/HeroLights.jsx
+++ src/components/models/hero_models/HeroLights.jsx
@@ -10,6 +10,8 @@ const HeroLights = () => (
       intensity={100}
       color="white"
     />
+
+
     {/* bluish overhead lamp */}
     <spotLight
       position={[4, 5, 4]}
@@ -18,6 +20,8 @@ const HeroLights = () => (
       intensity={40}
       color="#4cc9f0"
     />
+
+
     {/* purplish side fill */}
     <spotLight
       position={[-3, 5, 5]}
@@ -26,6 +30,8 @@ const HeroLights = () => (
       intensity={60}
       color="#9d4edd"
     />
+
+
     {/* area light for soft moody fill */}
     <primitive
       object={new THREE.RectAreaLight("#a259ff", 8, 3, 2)}
@@ -33,6 +39,8 @@ const HeroLights = () => (
       rotation={[-Math.PI / 4, Math.PI / 4, 0]}
       intensity={15}
     />
+
+
     {/* subtle point light for atmospheric tone */}
     <pointLight position={[0, 1, 0]} intensity={10} color="#7209b7" />
     <pointLight position={[1, 2, -2]} intensity={10} color="#0d00a4" />
