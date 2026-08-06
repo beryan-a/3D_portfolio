26a08a57696ce26bedb4f84b97ab14825f7ee897
Beryan Albeik <beryanalbeik@gmail.com>
2026-08-06 15:10:26 +0300

    particles

--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -4,6 +4,7 @@ import { useMediaQuery } from 'react-responsive'
 import { Room } from './Room'
 import HeroLights from './HeroLights'
 import React from 'react'
+import Particles from './Particles'
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({ query: '(max-width: 1024px)' }); // Adjust the breakpoint as needed
