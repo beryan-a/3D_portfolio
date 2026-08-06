eb05ec842375ed9f1bf326000a402e043583e31a
Beryan Albeik <beryanalbeik@gmail.com>
2026-08-06 11:20:29 +0300

    HeroExperience is a new component that has been added to the Hero section of the application. It is imported from the 'src/components/models/hero_models/HeroExperience' file and is used to display a 3D model or visual in the Hero section. The previous commented-out import statement for HeroExperience has been removed, and the component is now being rendered within the Hero section's layout.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import gsap from "gsap";
 //import AnimatedCounter from "../components/AnimatedCounter";
 import Button from "../components/Button";
 import { words } from "../constants";
-//import HeroExperience from "../components/models/hero_models/HeroExperience";
+import HeroExperience from "../components/models/hero_models/HeroExperience";
 
 const Hero = () => {
   useGSAP(() => {
@@ -71,7 +71,7 @@ const Hero = () => {
         </figure>
       </div>
 
-      <AnimatedCounter />
+      {/* <AnimatedCounter /> */}
     </section>
   );
 };
